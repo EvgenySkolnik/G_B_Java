8c7d693ce3a087eb3e55e4c36fc5d7e4883da39f
Eskolnik <eskolnik2021@Yandex.ru>
2021-07-30 20:57:57 +0300

    Home work7

---
+++ homeWork7/src/ru/gb/homeWork7/MainClass.java
@@ -0,0 +1,27 @@
+package ru.gb.homeWork7;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        Cat[] cat = new Cat[3];
+        cat[0] = new Cat("Barsik", 5);
+        cat[1] = new Cat("Murzik", 5);
+        cat[2] = new Cat("Begimot", 5);
+        Plate plate = new Plate(10);
+
+        System.out.println("Наши три кота " + cat[0].getName() + ", " + cat[1].getName() + ", " + cat[2].getName() + ", ");
+        plate.info();
+        for (int i = 0; i < 3; i++) {
+            cat[i].eat(plate);
+            if (cat[i].getSatiety()) {
+                System.out.println("Кот " + cat[i].getName() + " Сыт");
+            } else {
+                System.out.println("Кот " + cat[i].getName() + " Остался голодный.");
+                System.out.println("Наполним тарелку");
+                plate.addFood(15);
+            }
+        }
+
+        plate.info();
+    }
+}
---
+++ homeWork7/src/ru/gb/homeWork7/Plate.java
@@ -0,0 +1,30 @@
+package ru.gb.homeWork7;
+
+public class Plate {
+
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public boolean decreaseFood(int n) {
+        if (food - n >= 0) {
+            food = food - n;
+            return true;
+        } else {
+            System.out.println("low food level");
+            return false;
+        }
+    }
+
+    public void addFood(int food) {
+        this.food = this.food + food;
+    }
+
+    public void info() {
+        System.out.println("plate : " + food);
+    }
+
+
+}
