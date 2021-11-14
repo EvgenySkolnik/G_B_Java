72292e89978d779511bd2a5fd5791a1e6aea088c
Eskolnik <eskolnik2021@Yandex.ru>
2021-11-15 00:44:12 +0300

    Java 3 HomeWork 1 for verification

---
+++ Java3HW1/src/ru/gb/java3/hw1/Fruit.java
@@ -0,0 +1,16 @@
+package ru.gb.java3.hw1;
+
+public abstract class Fruit {
+
+    private float mass;
+
+    public Fruit(float mass) {
+        this.mass = mass;
+    }
+
+    float getWeight() {
+        return mass;
+    }
+
+
+}
